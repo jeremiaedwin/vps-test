c48855c3ac980e71e6a572d2b680c65baeaf6bea
jeremiaedwin <edwinjeremia00@gmail.com>
2025-03-30 23:36:43 +0700

    first commit

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        // Get all products
+        $products = Product::latest()->paginate(5);
+
+        return new ProductResource(true, 'List Data Products', $products);
+    }
+
+
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return void
+     * 
+     */
+    public function store(Request $request)
+    {
+        try {
+            // Define validation rules
+            $validator = Validator::make($request->all(), [
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'title' => 'required',
+                'description' => 'required',
+                'price' => 'required|numeric',
+                'stock' => 'required|numeric'
+            ]);
+            // Check if validation fails
+            if ($validator->fails()) {
+                return response->json($validator->errors(), 422);
+            }
+            
+            // Upload image
+            $image = $request->file('image');
+            $image->storeAs('products', $image->hashName());
+
+            // Create Product
+            $product = Product::create([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock
+            ]);
+
+            // Return response
+            return new ProductResource(true, 'Data Product Berhasil Ditambahkan', $product);
+        } catch (Exception $e) {
+            return response->json($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * show
+     * 
+     * @param mixed $id
+     * @return void
+     */
+    public function show($id)
+    {
+        try {
+            // Find product by product_id
+            $product = Product::find($id);
+
+            // Return single product as a resource
+            return new ProductResource(true, 'Detail Data Product!', $product);
+        } catch (\Exception $e) {
+            return response->json([$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * update
+     * 
+     * @param mixed $request
+     * @param mixed $id
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            
+            // Define validation rules
+            $validator = Validator::make($request->all(), [
+                'image'         => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'title'         => 'required',
+                'description'   => 'required',
+                'price'         => 'required|numeric',
+                'stock'         => 'required|numeric'
+            ]);
+            
+            // Check if validation fail
+            if ($validator->fails()) {
+                return response->json($validator->errors(), 422);
+            }
+            
+
+            // Find product by ID
+            $product = Product::find($id);
+
+
+            // Check if image has not empty
+            if ($request->hasFile('image')) {
+                // Delete old image
+                Storage::delete('products/' . basename($product->image));
+
+                // Upload image
+                $image = $request->file('image');
+                $image->storeAs('products', $image->hashName());
+
+                // Update product with new image
+                $product->update([
+                    'image'         => $image->hashName(),
+                    'title'         => $request->title,
+                    'description'   => $request->description,
+                    'price'         => $request->price,
+                    'stock'         => $request->stock
+                ]);
+            } else {
+                // Update product without image
+                $product->update([
+                    'title'         => $request->title,
+                    'description'   => $request->description,
+                    'price'         => $request->price,
+                    'stock'         => $request->stock
+                ]);
+            }
+
+            return new ProductResource(true, 'Data Product Berhasil Diubah!', $product);
+        } catch (\Exception $e) {
+            return response->json([$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * destroy
+     * 
+     * @param mixed $id
+     * @return void
+     */
+    public function destroy($id) 
+    {
+        try {
+            // Find Product By Id
+            $product = Product::find($id);
+            if (!$product){
+                return response->json(['success' => false, 'message' => 'Product not found!'], 404);
+            }
+            
+            // Delete image
+            Storage::delete('products/' . basename($product->image));
+
+            // Delete product
+            $product->delete();
+
+            // Return response
+            return new ProductResource(true, 'Data Product Berhasil Dihapus!', null);
+        } catch (\Exception $e) {
+            return response->json([$e->getMessage()], 500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method "GET"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+/**
+ * route "/products"
+ * @method "GET"
+ */
+Route::apiResource('/products', ProductController::class);
+Route::post('/products/{id}', [ProductController::class, 'update'])->name('update');
+Route::post('/products/{id}/delete', [ProductController::class, 'delete'])->name('delete');
